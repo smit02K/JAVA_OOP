7d7f10f5891dd284eb4fd7f39e7b42f087fc6451
smit02K <kalathiyasmit2001@gmail.com>
2023-07-31 10:41:32 +0530

    new change

--- Q6.java
+++ Q6.java
@@ -1,28 +1,28 @@
-package oopsmock;
+package oopsmock;//nothing 
 
 public class Q6 {
 	public static void main(String[] args) {
 
-		//method over loading
-		add_calling add_obj =new add_calling();
+		// method over loading
+		add_calling add_obj = new add_calling();
 		add_obj.add();
 		add_obj.add(10);
-		add_obj.add(20,30);
+		add_obj.add(20, 30);
 	}
 }
 
-
-//method over loading
-class add_calling{
-
+// method over loading
+class add_calling {
 
 	public void add() {
 		System.out.println("no arg");
 	}
+
 	public void add(int a) {
 		System.out.println("one arg");
 	}
-	public void add(int a,int b) {
+
+	public void add(int a, int b) {
 		System.out.println("two arg");
 	}
 }
